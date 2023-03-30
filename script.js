f6e6847797e469c18ef637b6e2bc1b476d4e4328
Belen Andrade <belenandrade2000@gmail.com>
2023-03-30 14:55:57 -0700

    got lat and lon coordinates, need to print results on page

--- script.js
+++ script.js
@@ -1,51 +1,69 @@
 
 var cityUrl = "http://api.openweathermap.org/geo/1.0/direct?q=cancun&appid=a8aad994de826937e53a4f435e492ae4"
-var weatherURL = "https://api.openweathermap.org/data/2.5/forecast?lat=21.1617854&lon=-86.8510468&units=imperial&appid=a8aad994de826937e53a4f435e492ae4"
+var weatherURL = "https://api.openweathermap.org/data/2.5/forecast?lat=21.1617854&lon=-86.8510468&units=imperial&cnt=1&appid=a8aad994de826937e53a4f435e492ae4"
 
-var searchButton = document.querySelector(".search-bar")
+var searchButton = document.querySelector("#weatherbtn")
+var input = document.querySelector("#city-input");
+// gives today's date using dayjs
 var today = dayjs();
 $("#date").text(today.format('MMM D, YYYY'));
 console.log(today)
 
+// getting latitude and longitude parameters
+// function getCityParams() {
+//     var searchParamsArr = document.location.search.split("&");
+//     var city=searchParamsArr[0].split("=").pop()
+//     searchApi(city);
+// }
 
-function getParams() {
-    var searchParamsArr = document.location.search.split("&");
-    var city=searchParamsArr[0].split("=").pop()
-    searchApi(city);
-}
-
-function searchApi(city){
-    var locCityUrl = "http://api.openweathermap.org/geo/1.0/direct?q="+city+"&appid=a8aad994de826937e53a4f435e492ae4"
+function searchApi(){
+    var locCityUrl = "http://api.openweathermap.org/geo/1.0/direct?q="+input.value+"&appid=a8aad994de826937e53a4f435e492ae4"
 
     fetch(locCityUrl)
         .then(function (response) {
         return response.json();
 })
     .then(function (data) {
-    //Using console.log to examine the data
-var latitude = document.createElement('h3');
-var longitude = document.createElement('h3');
+    
+var lat = document.createElement('h3');
+var lon = document.createElement('h3');
 
     console.log(data);
-    latitude.textContent = data.lat;
-    longitude.textContent = data.lon;
-    console.log(latitude);
-    console.log(longitude)
+    lat.textContent = data[0].lat;
+    lon.textContent = data[0].lon;
+    console.log(lat);
+    console.log(lon)
+weatherAPI(data[0].lat,data[0].lon)
 
 })
 };
 
+// getting weather parameters
+function getWeatherParams() {
+    var searchParamsArr = document.location.search.split("&");
+    var lat = searchParamsArr[0].split("=").pop()
+    var lon = searchParamsArr[1].split ("=").pop()
+    weatherAPI(lat,lon)
+}
 
+function weatherAPI(lat,lon) {
+    var cityWeatherUrl = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon="+ lon + "&units=imperial&appid=a8aad994de826937e53a4f435e492ae4"
+    fetch(cityWeatherUrl)
+    .then(function (response) {
+        return response.json();
+})
+    .then(function (data) {
+        console.log(data)
+        var temp = document.append(".temp")
+        var wind = document.append(".wind")
+        var humidity = document.append(".humidity")
 
-
-    // //Setting the text of the h3 element and p element.
-    // userName.textContent = data[i].login;
-    // userUrl.textContent = data[i].url;
-
-    // //Appending the dynamically generated html to the div associated with the id="users"
-    // //Append will attach the element as the bottom most child.
-    // usersContainer.append(userName);
-    // usersContainer.append(userUrl);
+        temp.textContent = data.temp;
+        wind.textContent = data.wind;
+        humidity.textContent = data.humidity;
+})}
+  
+// search button function
 function handleSearchButton (event) {
     event.preventDefault();
 
@@ -54,6 +72,6 @@ function handleSearchButton (event) {
     searchApi(cityInput);
 }
 
-searchButton.addEventListener("submit",handleSearchButton)
+searchButton.addEventListener("click",handleSearchButton)
 
-getParams();
+//getParams();
